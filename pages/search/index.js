f788b1fcc983e114b138c364d39cb2582ae94407
Zhangxuxuan <zhangxuxuan@itcast.cn>
2020-01-16 19:05:14 +0800

    二次提交，完成首页页面

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,94 +1,66 @@
+// pages/search/index.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    // 轮播图
-    swiper_list:[],
-    // 分类
-    classify:[],
-    // 推荐
-    recommend:[]
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    wx.request({
-      url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
-     success:(res)=>{
-       this.setData({
-         swiper_list:res.data.message
-       })
-     }
-    })
-    wx.request({
-      url: 'https://api.zbztb.cn/api/public/v1/home/catitems',
-      success:res=>{
-       this.setData({
-         classify:res.data.message
-       })
-      }
-    })
-    wx.request({
-      url: 'https://api.zbztb.cn/api/public/v1/home/floordata',
-      success:res=>{
-        console.log(res)
-       this.setData({
-         recommend:res.data.message
-       })
-      }
-    })
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   }
 })
